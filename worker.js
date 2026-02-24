03e1ccfe32bda2c6a1320a0abb94bd70263e522a
sgh <esghaier.ainformation@gmail.com>
2026-02-24 15:30:57 +0100

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "fsgf-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/css/style.css",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/favicon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
